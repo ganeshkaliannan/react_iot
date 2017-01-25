253f1bc878e6d12a77f1dfe6bf7a937549d3a8ab
ganesh kaliannan <ganesh.kaliannan@gmail.com>
2017-01-25 10:49:02 +0530

    style path updates

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header.jsx';
 import Home from './Home.jsx';
 import Footer from './Footer.jsx';
-require("./Style.css");
+require("../../styles/Style.css");
 
 class App extends React.Component {
 	constructor(props) {
